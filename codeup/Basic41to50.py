ec175edbdb11972f8715dac71ef74d98bf25608f
henhen7 <136338584+henhen7@users.noreply.github.com>
2024-04-02 17:17:35 +0900

    24-04-02

--- codeup/Basic41to50.py
+++ codeup/Basic41to50.py
@@ -1,1 +1,46 @@
 #41
+a, b = map(int, input().split())
+print(a%b)
+
+#42
+a = float(input())
+print(format(a, ".2f"))
+
+#43
+f1, f2 = map(float, input().split())
+print(format(f1/f2, ".3f"))
+
+#44
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print(format(float(a / b), ".2f"))
+
+#45
+a, b, c = map(int, input().split())
+sum = a + b + c
+mean = float(sum/3)
+print(sum, format(mean, ".2f"))
+
+#46
+a = int(input())
+print(a<<1)
+
+#47
+a, b = map(int, input().split())
+print(a<<b)
+
+#48
+a, b = map(int, input().split())
+print(a<b)
+
+#49
+a, b = map(int, input().split())
+print(a==b)
+
+#50
+a, b = map(int, input().split())
+print(a<=b)
