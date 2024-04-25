e0dc95756011fd07095581d2f17173d135ddf61d
henhen7 <136338584+henhen7@users.noreply.github.com>
2024-04-25 22:12:21 +0900

    24-04-24

--- barkingdog/DFS.py
+++ barkingdog/DFS.py
@@ -2,7 +2,7 @@
 #반복 팩토리얼
 def factorial_iterative(n):
     result = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         result *= i
     return result
 
@@ -10,4 +10,4 @@ def factorial_iterative(n):
 def factorial_recursive(n):
     if n <= 1:
         return 1
-    return n * factorial_recursive(n-1)
+    return n * factorial_recursive(n - 1)
