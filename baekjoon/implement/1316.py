506df8e9fe44ecb7d93e7d05f7e4ee1ce6f6a7e8
henhen7 <eatho@naver.com>
2024-06-24 10:47:43 +0900

    24-06-22

--- baekjoon/implement/1316.py
+++ baekjoon/implement/1316.py
@@ -1,13 +1,21 @@
 # # 1316
 # import sys
 #
-# n = int(sys.stdin.readline())
+# n = int(input())
 # word = [sys.stdin.readline().strip() for i in range(n)]
 # answer = 0
+# print(word)
 #
-# for i in len(word):
-#     print(i)
-#
+# for i in word:
+#     for j in range(len(word)-1):
+#         print(j)
+#         if word[j] != word[j+1]:
+#             new_word = word[j+1:]
+#             if word[j] in new_word:
+#               answer += 1
+#               break
+# print(n - answer)
+
 n = int(input())
 word = []
 answer = 0
@@ -17,7 +25,6 @@ for i in range(n):
         if word[j] != word[j+1]:
             new_word = word[j+1:]
             if word[j] in new_word:
-                answer -= 1
+                answer += 1
                 break
-    answer += 1
-print(answer)
+print(n - answer)
