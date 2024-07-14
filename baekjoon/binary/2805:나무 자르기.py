c58e6c9675271acf6c0a8d05e347a70be2e445f5
henhen7 <eatho@naver.com>
2024-07-15 00:59:08 +0900

    24-07-15

---
+++ baekjoon/binary/2805:나무 자르기.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+tree = sorted(list(map(int, input().split())))
+
+# 자른 나무 길이
+def cal_wood(height):
+    length = 0
+    for i in range(len(tree)):
+        if tree[i] >= height:
+            length += (tree[i]-height)
+    return length
+
+# 이분탐색
+def binary(tree, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        length = cal_wood(mid)
+        if length < target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return end
+
+print(binary(tree, m, 0, tree[-1]))
