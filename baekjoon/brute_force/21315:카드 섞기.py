3e4b4d46b8ff97a8d426717b3083ddfac5edde1e
henhen7 <eatho@naver.com>
2024-09-20 01:23:43 +0900

    24-09-20

--- baekjoon/brute_force/21315:카드 섞기.py
+++ baekjoon/brute_force/21315:카드 섞기.py
@@ -1,4 +1,4 @@
-
+# 시간 초과...
 n = int(input())
 li = list(map(int, input().split()))
 
@@ -9,10 +9,8 @@ def shuffle(card, idx):
     # 올리는 카드의 개수가 두배씩 줄어듬
     return shuffle(div_card, idx//2) + card[:len(card)-idx]
 
-result = []
 card_li = [i for i in range(1, n + 1)]
 for k1 in range(1, n + 1):
     for k2 in range(1, n + 1):
-        result.append(shuffle(shuffle(card_li, k1), k2))
         if shuffle(shuffle(card_li, 2 ** k1), 2 ** k2) == li:
             print(k1, k2)
