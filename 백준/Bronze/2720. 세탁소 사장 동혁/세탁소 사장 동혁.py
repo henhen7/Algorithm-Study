1442e005f31951d41a5e64c20d6234ba4d2a9831
헨헨7 <eatho@naver.com>
2024-11-12 15:00:53 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,12 @@
+n = int(input())
+coins = [25, 10, 5, 1]
+ans = []
+
+for _ in range(n):
+    c = int(input())
+    for coin in coins:
+        ans.append(c // coin)
+        c = c % coin
+
+for i in range(0, len(ans), 4):
+    print(*ans[i:i+4])
